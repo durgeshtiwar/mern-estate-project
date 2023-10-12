241022b7ababc32b7758de5563a37172c7f644af
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2023-10-12 20:51:46 +0530

    complete sign up page functionality

--- api/index.js
+++ api/index.js
@@ -14,8 +14,8 @@ mongoose
 });
 const app = express();
 app.use(express.json());
-app.listen(3000,() => {
-    console.log('Server is running on port 3000!');
+app.listen(5020,() => {
+    console.log('Server is running on port 5020!');
     }
 );
 
