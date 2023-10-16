0187ca0d6d68e40d138b4b16fd359f1fe7b77a76
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2023-10-16 16:11:39 +0530

    add  sign out functionality

--- mega_project/src/pages/Profile.jsx
+++ mega_project/src/pages/Profile.jsx
@@ -7,7 +7,7 @@ import {
   uploadBytesResumable,
 } from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserStart,updateUserSuccess,updateUserFailure, deleteUserFailure, deleteUserSuccess, deleteUserStart } from '../redux/user/userSlice';
+import { updateUserStart,updateUserSuccess,updateUserFailure, deleteUserFailure, deleteUserSuccess, deleteUserStart, signOutUserStart } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 export default function Profile() {
   const fileRef = useRef(null);
@@ -104,6 +104,25 @@ export default function Profile() {
     }
   };
 
+
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(data.message));
+    }
+  };
+
+
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -170,7 +189,7 @@ export default function Profile() {
         >
           Delete account
         </span>
-        <span  className='text-red-700 cursor-pointer'>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>
           Sign out
         </span>
       </div>
